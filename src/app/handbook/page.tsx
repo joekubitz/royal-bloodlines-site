8b32170257cce49c74ac2ca5fd3453a32fb09c02
Joe Kubitz <joekubitz7@gmail.com>
2026-03-08 12:09:00 -0500

    added handbook

---
+++ src/app/handbook/page.tsx
@@ -0,0 +1,10 @@
+export default function HandbookPage() {
+  return (
+    <main className="w-full h-screen bg-black">
+      <iframe
+        src="/royals-bloodline-agent-handbook.pdf"
+        className="w-full h-full"
+      />
+    </main>
+  );
+}
